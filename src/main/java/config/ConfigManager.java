d13cbfa0cdc4ca97fd54b91ee9563e6d96e2b7ff
SoPlays <joao.ranger@gmail.com>
2021-04-15 01:16:29 +0100

    Writing and reading from file Created new nested class in MongodbCloudCollector called MongodbCloudCollectorData

--- src/main/java/config/ConfigManager.java
+++ src/main/java/config/ConfigManager.java
@@ -1,13 +1,45 @@
 package config;
 
-import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
+import mongodb.collectorwriter.MongodbCloudCollector;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+import static mongodb.collectorwriter.MongodbCloudCollector.MongodbCloudCollectorData.*;
 
 public class ConfigManager {
+    private static final String[] COMMENT =
+            {"//Nome do utilizador da base de dados\n",
+            "//User a usar na base de dados\n",
+            "//Nome da database para o mongo conectar\n",
+            "//Ip a usar","//Porta a usar para efetuar a configuração\n",
+            "//Password a usar para entrar no mongodb\n",
+            "//Nome das coleções a clonar, separadas por \";\"\n"};
 
-    public static void writeToFile(String filename, List<String> toWrite){
-        try (
+    public static void writeToFile(String filename, MongodbCloudCollector.MongodbCloudCollectorData toWrite) {
+        try (FileWriter myWriter = new FileWriter("filename.txt")) {
+            clearConfigFile(filename);
+            myWriter.write(COMMENT[0]);
+            myWriter.write(toWrite.getUser() + "\n");
+            myWriter.write(COMMENT[1]);
+            myWriter.write(toWrite.getDatabaseUsers() + "\n");
+            myWriter.write(COMMENT[2]);
+            myWriter.write(toWrite.getDatabase() + "\n");
+            myWriter.write(COMMENT[3]);
+            myWriter.write(toWrite.getIp() + "\n");
+            myWriter.write(COMMENT[4]);
+            myWriter.write(toWrite.getPort() + "\n");
+            myWriter.write(COMMENT[5]);
+            myWriter.write(toWrite.getPassword() + "\n");
+            myWriter.write(COMMENT[6]);
+            myWriter.write(toWrite.getCollections() + "\n");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        /*try (
             FileOutputStream fileOut = new FileOutputStream(filename);
             ObjectOutputStream out = new ObjectOutputStream(fileOut);)
         {
@@ -19,23 +51,69 @@ public class ConfigManager {
         } catch (IOException ioException) {
             ioException.printStackTrace();
         }
+        */
 
     }
-    public static List<String> readFromFile(String filename){
-        List<String> output = new ArrayList<>();
+
+    public static MongodbCloudCollector.MongodbCloudCollectorData readFromFile(String filename) throws NumberFormatException, IllegalArgumentException {
+        MongodbCloudCollector.MongodbCloudCollectorData data = new MongodbCloudCollector.MongodbCloudCollectorData();
+        Scanner scanner = new Scanner(filename);
+        String line;
+        while (scanner.hasNextLine()) {
+            line = scanner.nextLine();
+            if (!line.startsWith("//")) {
+                String[] lineContent = line.split("=");
+                lineContent[1]=lineContent[1].trim().replace("\n","");
+                switch (lineContent[0]) {
+                    case USER:
+                        data.setUser(lineContent[1]);
+                        break;
+                    case DATABASEUSERS:
+                        data.setDatabaseUsers(lineContent[1]);
+                        break;
+                    case DATABASE:
+                        data.setDatabase(lineContent[1]);
+                        break;
+                    case IP:
+                        data.setIp(lineContent[1]);
+                        break;
+                    case PORT:
+                        data.setPort(Integer.parseInt(lineContent[1]));
+                        break;
+                    case PASSWORD:
+                        data.setPassword(lineContent[1].toCharArray());
+                        break;
+                    case COLLECTIONS:
+                        data.setCollections(lineContent[1].split(";"));
+                        break;
+                    default:
+                        throw new IllegalArgumentException();
+                }
+
+            }
+
+        }
+        /*
         try (
             FileInputStream fileIn = new FileInputStream(filename);
             ObjectInputStream in = new ObjectInputStream(fileIn))
         {
-
-
+            output = (ArrayList<String>)in.readObject();
 
         } catch (FileNotFoundException fileNotFoundException) {
             fileNotFoundException.printStackTrace();
         } catch (IOException ioException) {
             ioException.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
         }
-        return output;
+        */
+        return data;
+    }
+
+    //TODO
+    public static void clearConfigFile(String filename) throws IOException {
+        new FileWriter(filename, false).close();
 
     }
 }
--- src/main/java/mongodb/collectorwriter/MongodbCloudCollector.java
+++ src/main/java/mongodb/collectorwriter/MongodbCloudCollector.java
@@ -11,15 +11,21 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MongodbCloudCollector {
+
     final static String user = "aluno";
     final static String databaseUsers = "admin";
     final static String database = "sid2021";
     final static String ip = "194.210.86.10";
     final static int port = 27017;
-    final static char[] password={'a','l','u','n','o'};
-    final static String[] collections={"sensorh1","sensorh2","sensorl1","sensorl2","sensort1","sensort2"};
+    final static char[] password = {'a', 'l', 'u', 'n', 'o'};
+    final static String[] collections = {"sensorh1", "sensorh2", "sensorl1", "sensorl2", "sensort1", "sensort2"};
+    private MongodbCloudCollectorData data;
 
-    public static void collect(){
+    public MongodbCloudCollector(MongodbCloudCollectorData data) {
+        this.data = data;
+    }
+
+    public static void collect() {
         try {
             writeInfo(createClient().getDatabase(database));
         } catch (InterruptedException e) {
@@ -28,7 +34,7 @@ public class MongodbCloudCollector {
         }
     }
 
-    private static MongoClient createClient(){
+    private static MongoClient createClient() {
         MongoCredential credential = MongoCredential.createCredential(user, databaseUsers, password);
         MongoClient mongoClient = new MongoClient(new ServerAddress(ip, port), Arrays.asList(credential));
         return mongoClient;
@@ -38,9 +44,9 @@ public class MongodbCloudCollector {
 
         ArrayList<MongodbLocalWriter> writers = new ArrayList<>();
 
-     //   System.out.println("Started writing collections");
+        //   System.out.println("Started writing collections");
 
-        for(String collection: collections){
+        for (String collection : collections) {
             MongoCollection<Document> table = db.getCollection(collection);
             MongodbLocalWriter mongodbLocalWriter = new MongodbLocalWriter(collection, table);
             writers.add(mongodbLocalWriter);
@@ -52,6 +58,88 @@ public class MongodbCloudCollector {
         }
 
         System.out.println("Finished writing collections");*/
+
+    }
+
+    public MongodbCloudCollectorData getData() {
+        return data;
+    }
+
+    public void setData(MongodbCloudCollectorData data) {
+        this.data = data;
+    }
+
+    public static class MongodbCloudCollectorData {
+        public static final String USER = "user";
+        public static final String DATABASEUSERS = "databaseUsers";
+        public static final String DATABASE = "database";
+        public static final String IP = "ip";
+        public static final String PORT = "port";
+        public static final String PASSWORD = "password";
+        public static final String COLLECTIONS = "collections";
+        private String user;
+        private String databaseUsers;
+        private String database;
+        private String ip;
+        private int port;
+        private char[] password;
+        private String[] collections;
+
+        public String getUser() {
+            return user;
+        }
+
+        public void setUser(String user) {
+            this.user = user;
+        }
+
+        public String getDatabaseUsers() {
+            return databaseUsers;
+        }
+
+        public void setDatabaseUsers(String databaseUsers) {
+            this.databaseUsers = databaseUsers;
+        }
+
+        public String getDatabase() {
+            return database;
+        }
+
+        public void setDatabase(String database) {
+            this.database = database;
+        }
+
+        public String getIp() {
+            return ip;
+        }
+
+        public void setIp(String ip) {
+            this.ip = ip;
+        }
+
+        public int getPort() {
+            return port;
+        }
+
+        public void setPort(int port) {
+            this.port = port;
+        }
+
+        public char[] getPassword() {
+            return password;
+        }
+
+        public void setPassword(char[] password) {
+            this.password = password;
+        }
+
+        public String[] getCollections() {
+            return collections;
+        }
+
+        public void setCollections(String[] collections) {
+            this.collections = collections;
+        }
 
     }
 
